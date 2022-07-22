45c5751484d53e39ea00b9a8713748a610463464
Jon Bird <attid@Jons-Laptop>
2022-07-22 15:26:24 -0600

    working on index.js

--- index.js
+++ index.js
@@ -1,8 +1,145 @@
 const inquirer = require('inquirer');
 const generateReadme = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your github repository name (use dash instead of spaces)(Case Sensitive)(Required)',
+        validate: titleInput => {
+          if (titleInput) {
+            return true;
+          } else {
+            console.log('Please enter your GitHub repository!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your E-mail!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a project description (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter  a project description!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter a Live Link to the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a Live Link to the project');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instructions for the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter installation instructions for the project');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'useage',
+        message: 'Enter usage information for the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter usage information for the project');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution information for the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter contribution information for the project');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test information for the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter test information for the project');
+            return false;
+          }
+        }
+      },{
+        type: 'list',
+        name: 'license',
+        message: 'What license do you have selected for this project? (Check all that apply)',
+        choices: ['MIT License', 'Apache License v3.0', 'GNU General Public License v3.0', 'BSD-2-Clause', 'BSD-3-Clause', 'Boost Software License', 'Creative Commons Zero v1.0 Universeal', 'Eclipse Public License v2.0', 'GNU affero General Public License v3.0', 'GNU general Public license v2.0', 'GNU Lesser General Public License v2.1']
+      },
+    ]);
+  };
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (!license) {
+function renderLicenseBadge(data) {
+  if (!data.license) {
     return ''
   }
   return `
-  ![LicenseBadge](https://img.shields.io/github/license/${githubUsername}/${Repository})
+  ![LicenseBadge](https://img.shields.io/github/license/${data.github}/${data.title})
   `;
 }
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (!license) {
+function renderLicenseLink(data) {
+  if (!data.license) {
     return ''
   }
   return `
@@ -23,20 +23,59 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
+function renderLicenseSection(data) {
+  if (!data.license) {
     return ''
   }
   return `
   ## License
-  
-  ${license}
+
+  ${data.license}
   `;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data)}
+
+  ## Description
+  ${data.description}
+
+  ## Live Link
+  ${data.link}
+
+  ## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  ${renderLicenseLink(data)}
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.useage}
+
+ 
+  ${renderLicenseSection(data)}
+
+ 
+  ## Contributing
+  ${data.contributing}
+
+ 
+  ## Tests
+  ${data.tests}
+
+ 
+  ## Questions
+  You can reach me on with my [github profile](https://github.com/${data.github})
+   or reach me by [Email](${data.email})
+
 
 `;
 }
